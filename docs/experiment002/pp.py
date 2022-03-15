e3e28ffc2dbd6e086f83316fa5788287ed4f551c
mgharafi <mohamed.gharafi@inria.fr>
2022-03-15 23:02:29 +0100

    restructure experiment folder

--- docs/experiment002/pp.py
+++
@@ -1,4 +0,0 @@
-import cocopp
-
-try: cocopp.main('exdata/fmin2_of_cma.evolution_strategy_2D_on_bbob')
-except: pass
--- docs/experiment004/sync-data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-
-rsync -a --exclude '__pycache__' --exclude 'slurm*' --exclude '*.pydict' -e "ssh -i $HOME/.ssh/id_mar" mgharafi@margaret.saclay.inria.fr:./dd-cma-coco-benchmark/experiments/$(basename $(pwd)) ../
